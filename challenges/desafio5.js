69a5394b41fd68f17482c6f62c3bc665f26b0c33
Bruno Pinho <brunopinho@outlook.com>
2021-11-24 18:52:48 -0300

    feito requisito 7

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,7 +1,9 @@
-db.movies.aggregate([
-  {
-    $match: {
-      awards: { $in: ["Won 1 Oscar"] },
-    },
-  },
-]);
+// const FAV_ACTORS = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
+
+// db.movies.aggregate([
+//   {
+//     $project: {
+//       test: { $setIntersection: ["$FAV_ACTORS", "$cast"] },
+//     },
+//   },
+// ]);
