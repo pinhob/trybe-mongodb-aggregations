0805d74eba66bec7ed5d44ba0e8b5e776b5f6878
Bruno Pinho <brunopinho@outlook.com>
2021-11-25 18:52:59 -0300

    feito requisito 05

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,9 +1,45 @@
-// const FAV_ACTORS = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
+const FAV_ACTORS = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
 
-// db.movies.aggregate([
-//   {
-//     $project: {
-//       test: { $setIntersection: ["$FAV_ACTORS", "$cast"] },
-//     },
-//   },
-// ]);
+db.movies.aggregate([
+  {
+    $match: {
+      countries: "USA",
+      "tomatoes.viewer.rating": { $gte: 3 },
+      cast: { $exists: true },
+    },
+  },
+  {
+    $addFields: {
+      hasFavActors: { $setIntersection: ["$cast", FAV_ACTORS] },
+    },
+  },
+  {
+    $match: {
+      hasFavActors: { $ne: [] },
+    },
+  },
+  {
+    $addFields: {
+      num_favs: { $size: "$hasFavActors" },
+    },
+  },
+  {
+    $sort: {
+      num_favs: -1,
+      "tomatoes.viewer.rating": -1,
+      title: -1,
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+    },
+  },
+  {
+    $skip: 24,
+  },
+  {
+    $limit: 1,
+  },
+]);
